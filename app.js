a3d75192c21299dac89a21bb883cf1bd35a9c09d
Altamash Akhter <altamash360@gmail.com>
2017-05-27 11:57:50 +0500

    angular
    angular app

---
+++ app.js
@@ -0,0 +1,7 @@
+var myNinjaApp = angular.module('myNinjaApp', []);
+
+myNinjaApp.controller('Controller1', ['$scope', function($scope){
+	$scope.message = "hey u all";
+	$scope.ninjas1 = ['yoshi', 'ryu', 'shaun', 'crystal'];
+  $scope.numbers = ['1', '2', '3', '4', '5'];
+}]);
